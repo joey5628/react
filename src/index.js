f6044d4bdbc7b63143595c12770f2ba32ae97bde
yi.z <yi.z@Ctrip.com>
2015-06-02 10:19:42 +0800

    react update by window

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-React.render(
-	<h1>hello world!</h1>,
-	document.getElementById('container')
-);
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <p>
+        Hello, <input type="text" placeholder="Your name here" />!
+        It is {this.props.date.toTimeString()}
+      </p>
+    );
+  }
+});
+
+setInterval(function() {
+  React.render(
+    <HelloWorld date={new Date()} />,
+    document.getElementById('container')
+  );
+}, 500);
